3979a39c5066b8e2d0cdf738c9a886d19b17c9b9
RoBoT_095 <38968333+RoBoT095@users.noreply.github.com>
2023-04-05 13:40:09 -0400

    Animate palette deletion
    Also changed PaletteList from a class to a function

--- src/PaletteList.jsx
+++ src/PaletteList.jsx
@@ -1,32 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import "./styles/PaletteList.css";
 
-export default class PaletteList extends Component {
-    render() {
-        const { palettes, deletePalette } = this.props;
-        return (
-            <div className="PaletteList-root">
-                <div className="PaletteList-container">
-                    <nav className="PaletteList-nav">
-                        <h1 className="PaletteList-heading">React Colors</h1>
-                        <Link to="/palette/new" className="PaletteList-new">
-                            Create Palette
-                        </Link>
-                    </nav>
-                    <div className="PaletteList-palettes">
-                        {palettes.map((palette) => (
+export default function PaletteList(props) {
+    const { palettes, deletePalette } = props;
+    return (
+        <div className="PaletteList-root">
+            <div className="PaletteList-container">
+                <nav className="PaletteList-nav">
+                    <h1 className="PaletteList-heading">React Colors</h1>
+                    <Link to="/palette/new" className="PaletteList-new">
+                        Create Palette
+                    </Link>
+                </nav>
+                <TransitionGroup className="PaletteList-palettes">
+                    {palettes.map((palette) => (
+                        <CSSTransition
+                            key={palette.id}
+                            timeout={500}
+                            classNames="fade"
+                        >
                             <MiniPalette
                                 {...palette}
                                 key={palette.id}
                                 id={palette.id}
                                 handleDelete={deletePalette}
                             />
-                        ))}
-                    </div>
-                </div>
+                        </CSSTransition>
+                    ))}
+                </TransitionGroup>
             </div>
-        );
-    }
+        </div>
+    );
 }
